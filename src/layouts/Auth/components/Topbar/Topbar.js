f3f3fb859430f22d14dab44f3f35bec6486f9686
Ricardo De la Maza <radelamaza@uc.cl>
2021-05-17 12:48:01 -0400

    subo repo

---
+++ src/layouts/Auth/components/Topbar/Topbar.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/styles';
+import { AppBar, Toolbar } from '@material-ui/core';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    boxShadow: {
+      shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.8,
+    shadowRadius: 1, 
+    }
+  }
+}));
+
+const Topbar = props => {
+  const { className, ...rest } = props;
+
+  const classes = useStyles();
+
+  return (
+    <AppBar
+      {...rest}
+      className={clsx(classes.root, className)}
+      color="primary"
+    >
+      <Toolbar>
+        <RouterLink to="/">
+          <img
+            alt="Logo"
+            src="/images/logos/logo--red.png"
+            
+          />
+        </RouterLink>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+Topbar.propTypes = {
+  className: PropTypes.string
+};
+
+export default Topbar;
